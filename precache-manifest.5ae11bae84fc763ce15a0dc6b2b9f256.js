f46964d384ac37476f845102843bf7eb0faefc70
Raeya Vidhlaejandi <raeya.vidhlaejandi@softwire.com>
2020-02-26 17:57:53 +0000

    Updates

---
+++ precache-manifest.5ae11bae84fc763ce15a0dc6b2b9f256.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72a7ff5d609709fa120944a101e35685",
+    "url": "/prototypetrain/index.html"
+  },
+  {
+    "revision": "ac1347b9a60e84020bf9",
+    "url": "/prototypetrain/static/css/main.103134ab.chunk.css"
+  },
+  {
+    "revision": "0102ed32969f4cc5635c",
+    "url": "/prototypetrain/static/js/2.7489a8c4.chunk.js"
+  },
+  {
+    "revision": "967c3aa677d48917bda81b3b2ff2693c",
+    "url": "/prototypetrain/static/js/2.7489a8c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac1347b9a60e84020bf9",
+    "url": "/prototypetrain/static/js/main.c4f32f36.chunk.js"
+  },
+  {
+    "revision": "787611e2dbde6a8a2917",
+    "url": "/prototypetrain/static/js/runtime-main.f158e2e1.js"
+  },
+  {
+    "revision": "2f35a209bccf1145e1597759a36c34d2",
+    "url": "/prototypetrain/static/media/1ab-confirmation.2f35a209.png"
+  },
+  {
+    "revision": "418212f3874c7a6bfcbe99d212684f8f",
+    "url": "/prototypetrain/static/media/1ab-review.418212f3.png"
+  },
+  {
+    "revision": "c09f78464e7b4aee5fb8dae08fc66404",
+    "url": "/prototypetrain/static/media/1c-confirmation.c09f7846.png"
+  },
+  {
+    "revision": "de5e6a65498c33179a6bdffce8213f42",
+    "url": "/prototypetrain/static/media/1c-review.de5e6a65.png"
+  },
+  {
+    "revision": "6123a1a299c724e892749ac4d8a4798c",
+    "url": "/prototypetrain/static/media/1e-confirmation.6123a1a2.png"
+  },
+  {
+    "revision": "14d80ccdc94fcf905a555c3e7adc90d7",
+    "url": "/prototypetrain/static/media/1e-review.14d80ccd.png"
+  },
+  {
+    "revision": "32c5c341c324f736ab0e7f6a6b6ea24f",
+    "url": "/prototypetrain/static/media/2a-confirmation.32c5c341.png"
+  },
+  {
+    "revision": "105b218cd9f226e9a57757a39353537c",
+    "url": "/prototypetrain/static/media/2a-review.105b218c.png"
+  },
+  {
+    "revision": "cb628398b595168d803f2670a279236f",
+    "url": "/prototypetrain/static/media/2b-confirmation.cb628398.png"
+  },
+  {
+    "revision": "d800acaa98a98a123cd09906da850d8f",
+    "url": "/prototypetrain/static/media/2b-review.d800acaa.png"
+  },
+  {
+    "revision": "6655ed73746d15c37dbbfb2c0e854ead",
+    "url": "/prototypetrain/static/media/2b2-confirmation.6655ed73.png"
+  },
+  {
+    "revision": "e5b345a7648015f67c286fe91af9ead0",
+    "url": "/prototypetrain/static/media/2b2-review.e5b345a7.png"
+  },
+  {
+    "revision": "5c8f5e969225e9e2e2bdd769cca9fa48",
+    "url": "/prototypetrain/static/media/3ab-confirmation.5c8f5e96.png"
+  },
+  {
+    "revision": "b328a18ccf8afa18214d0fb5cc2eec60",
+    "url": "/prototypetrain/static/media/3ab-review.b328a18c.png"
+  },
+  {
+    "revision": "4efe66b7735599b207e789a7878aab87",
+    "url": "/prototypetrain/static/media/GothamSSm-Bold.4efe66b7.otf"
+  },
+  {
+    "revision": "5fd222f7a89fdc326fd4ab5d8671cdc9",
+    "url": "/prototypetrain/static/media/GothamSSm-Book.5fd222f7.otf"
+  },
+  {
+    "revision": "5bfea8de35795e081a79c81da04f558f",
+    "url": "/prototypetrain/static/media/confirmation.5bfea8de.png"
+  },
+  {
+    "revision": "98d085605694c209a7731ac16bc33002",
+    "url": "/prototypetrain/static/media/journeyInfo.98d08560.png"
+  },
+  {
+    "revision": "13d35289d7eff1bd63a2f6ff5d487514",
+    "url": "/prototypetrain/static/media/logo.13d35289.svg"
+  },
+  {
+    "revision": "f30d4edb6064351e44940e11b483781f",
+    "url": "/prototypetrain/static/media/payment.f30d4edb.png"
+  },
+  {
+    "revision": "b35e4f91fd43e172581d5215a38d00d9",
+    "url": "/prototypetrain/static/media/review-footer.b35e4f91.png"
+  },
+  {
+    "revision": "b83acf3864b0f5826b0d5dcea1181bfa",
+    "url": "/prototypetrain/static/media/review.b83acf38.png"
+  },
+  {
+    "revision": "af017db9b35c3f6314da53fb31ae27f3",
+    "url": "/prototypetrain/static/media/termsAndConditions.af017db9.png"
+  }
+]);

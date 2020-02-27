f939eaacd083d4f57750ec4e375bcb2a531fb1f6
Raeya Vidhlaejandi <raeya.vidhlaejandi@softwire.com>
2020-02-27 14:27:09 +0000

    Header fixed-ness and update to review footer

--- src/ui/ResultsHeader.js
+++ src/ui/ResultsHeader.js
@@ -8,35 +8,41 @@ export default class ResultsHeader extends React.Component {
 
     if (data.ticketType === 1 || data.ticketType === 2) {
       return (
-        <div className="ResultsHeader">
-          <div className={`ResultsHeader-group ${data.stage === 0 ? 'selected' : ''}`}>
-            <div className="ResultsHeader-title">
-              1 - Going out
+        <React.Fragment>
+          <div className="ResultsHeader">
+            <div className={`ResultsHeader-group ${data.stage === 0 ? 'selected' : ''}`}>
+              <div className="ResultsHeader-title">
+                1 - Going out
+              </div>
+              <div className="ResultsHeader-subtitle">
+                {data.time.outbound}
+              </div>
             </div>
-            <div className="ResultsHeader-subtitle">
-              {data.time.outbound}
+            <div className={`ResultsHeader-group ${data.stage === 1 ? 'selected' : ''}`}>
+              <div className="ResultsHeader-title">
+                2 - Coming back
+              </div>
+              <div className="ResultsHeader-subtitle">
+                {data.time.inbound}
+              </div>
             </div>
           </div>
-          <div className={`ResultsHeader-group ${data.stage === 1 ? 'selected' : ''}`}>
-            <div className="ResultsHeader-title">
-              2 - Coming back
-            </div>
-            <div className="ResultsHeader-subtitle">
-              {data.time.inbound}
-            </div>
-          </div>
-        </div>
+          <div className="ResultsHeader-padder" />
+        </React.Fragment>
       );
     }
 
     return (
-      <div className="ResultsHeader">
-        <div className="ResultsHeader-group selected">
-          <div className="ResultsHeader-subtitle">
-            {data.time.outbound}
+      <React.Fragment>
+        <div className="ResultsHeader ResultsHeader-small">
+          <div className="ResultsHeader-group selected">
+            <div className="ResultsHeader-subtitle">
+              {data.time.outbound}
+            </div>
           </div>
         </div>
-      </div>
+        <div className="ResultsHeader-padder-small" />
+      </React.Fragment>
     )
 
   }
